238d46e5a2ac183dbc269e12c716a70001d71b6c
Matheus Guedes de Sousa <matheusguedes.ufvjm.si@gmail.com>
2020-05-02 19:08:45 -0300

    logout e flash messages

--- src/routes.js
+++ src/routes.js
@@ -4,19 +4,34 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
+// Middlewares
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
 // Controllers
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
-routes.get('/', SessionController.create)
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
+
+  return next()
+})
+
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
 
-routes.get('/signup', UserController.create)
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.use('/app', authMiddleware)
 
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
   return res.render('dashboard')
 })
+
+routes.get('/app/logout', SessionController.destroy)
 
 module.exports = routes
